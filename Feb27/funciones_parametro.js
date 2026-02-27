f714e7b98d55878376dddd7760185f68be347ee3
Pablo <pabloreyesquina@gmail.com>
2026-02-27 14:14:16 +0100

    Actualización 27 Febrero

---
+++ Feb27/funciones_parametro.js
@@ -0,0 +1,48 @@
+/* function sumar(a, b) {
+  return a + b
+}
+
+function multiplicar(a, b) {
+  return a * b
+}
+
+function restar(a, b) {
+  return a - b
+}
+
+// Función que recibe una operación como parámetro
+function calcular(num1, num2, operacion) {
+  const resultado = operacion(num1, num2)
+  console.log('El resultado es: ' + resultado)
+  return resultado
+}
+
+// Usamos diferentes operaciones
+calcular(5, 3, sumar)       // El resultado es: 8
+calcular(5, 3, multiplicar) // El resultado es: 15
+calcular(5, 3, restar)      // El resultado es: 2 */
+
+/* Arrays */
+
+let animales = ["gato", "perro", "serpiente", "tortuga", "sardina"]
+console.log(animales[3])
+console.log(animales.length)
+console.log(animales)
+
+animales[1] = "caballo"
+
+console.log(animales)
+
+animales[5] = 10
+
+console.log(animales)
+
+animales[3]=1<0
+
+console.log(animales)
+
+animales[10]=6.5
+
+console.log(animales)
+
+console.log(animales.length)
